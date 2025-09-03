1cd91df87d5aa4d9bc6cd117279ba13d5834f5af
Tom Larsson <tmlsn@hotmail.com>
2025-09-03 11:38:58 +0200

    in progress

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,12 +1,25 @@
+import { useState } from 'react';
+import Square from './components/Square';
+
+const INITIAL_GAME_STATE = ['X', 'X', 'X', '', '', '', '', '', ''];
+
 function Game() {
+  const [gameState, setGameState] = useState(INITIAL_GAME_STATE);
+
   return (
-    <div className="h-full p-8 text-slate-800 bg-gradient-to-r from-cyan-500 to blue-500">
-      <h1 className="text-center text-5xl mb-4 front-display text-white">
+    <div className="h-full p-8 text-slate-800 bg-gradient-to-r from-cyan-500 to-blue-500">
+      <h1 className="text-center text-5xl mb-4 font-display text-white">
         Tic Tac Toe
       </h1>
-      <div>Board</div>
+      <div>
+        <div className="grid grid-cols-3 gap-3 mx-auto w-96">
+          {gameState.map((player, index) => (
+            <Square key={index}>{player}</Square>
+          ))}
+        </div>
 
-      <div>Scores</div>
+        <div>Scores</div>
+      </div>
     </div>
   );
 }
--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -1,5 +1,15 @@
-function Square() {
-  return <button></button>;
+import { ReactNode } from 'react';
+
+type Props = {
+  children: ReactNode;
+};
+
+function Square({ children }) {
+  return (
+    <div className="h-36 border-solid border-4 border-slate-200 font-display text-center flex text-7xl justify-center items-center cursor-pointer">
+      <span>{children}</span>
+    </div>
+  );
 }
 
 export default Square;
