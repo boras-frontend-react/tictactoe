7f032b54ca6bbc645bb29e1e16423bcdf27db7ec
Tom Larsson <tmlsn@hotmail.com>
2025-09-24 23:44:04 +0200

    remove css height

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Footer } from '@components';
 
 function App() {
   return (
-    <div className="min-h-screen flex flex-col bg-gradient-to-r from-green-800 to-green-500">
+    <div className="min-h-screen flex flex-col">
       <main className="flex-1 flex flex-col">
         <Game />
       </main>
